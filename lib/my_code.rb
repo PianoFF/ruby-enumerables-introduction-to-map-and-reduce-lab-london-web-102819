9fadfe68814f6718cd2238253fe5633515f860f6
Fangfei Chen <fangfei1star@gmail.com>
2019-10-23 14:58:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,8 +64,9 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true (source_array)
-  source_array.each do
-    if true
+  it_is = true
+  source_array.each do |i|
+    if i
       it_is = true
       break
     else 
